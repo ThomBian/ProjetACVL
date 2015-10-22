ea0355461993285004493fe2d91f750a8bb2950b
Arthur De Kimpe <arthurdekimpe@gmail.com>
2015-10-22 13:19:30 +0200

    added style initilization for state

---
+++ src/main/java/controller/command/CreateFinalState.java
@@ -0,0 +1,12 @@
+package controller.command;
+
+import controller.Diagram;
+
+public class CreateFinalState implements Command {
+
+	@Override
+	public void execute() {
+		Diagram.getInstance().createFinalState();		
+	}
+
+}
