1d667c63938ea462ef8948ba51570672541730cb
Arthur De Kimpe <arthurdekimpe@gmail.com>
2015-10-22 11:50:06 +0200

    Revert "Modified ui"
    This reverts commit a6033b530878e2307eed2a137424f9da9c168deb.

--- src/main/java/view/Command.java
+++
@@ -1,6 +0,0 @@
-package view;
-
-public interface Command {
-
-		void execute();
-}
--- src/main/java/view/MainView.java
+++ src/main/java/view/MainView.java
@@ -5,65 +5,55 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
-import javax.swing.JPanel;
 import javax.swing.UIManager;
-
-import com.mxgraph.view.mxGraph;
 
 public class MainView {
 
 	private JFrame frame;
 	private JMenuBar menuBar;
-	private GraphView graphView;
-	
-	private final Button createInitialStateButton = new Button("Create an initial state");
-	private final Button createStateButton = new Button("Create a state");
-	private final Button createCompositeStateButton = new Button("Create a composite state");
-	private final Button createFinalStateButton = new Button("Create a final state");
-	private final Button flattenButton = new Button("Flatten diagram");
-	private final Button validateButton = new Button("Validate diagram");
-
-
-	public JFrame getFrame() {
-		return frame;
-	}
-
-	public GraphView getGraph() {
-		return graphView;
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+			EventQueue.invokeLater(new Runnable() {
+				public void run() {
+					try {
+						MainView window = new MainView();
+						window.frame.setVisible(true);
+					} catch (Exception e) {
+						e.printStackTrace();
+					}
+				}
+			});
+		} catch (Exception e1) {
+			e1.printStackTrace();
+		}
+		
 	}
 
 	/**
 	 * Create the application.
 	 */
-	public MainView(mxGraph graph) {
+	public MainView() {
 		initialize();
-		initializeEmptyGraph(graph);
 	}
 
 	/**
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frame = new JFrame("Diagram editor");
-		frame.setBounds(100, 100, 750, 400);
+		frame = new JFrame();
+		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		
 		menuBar = new JMenuBar();
-		menuBar.add(createInitialStateButton);
-		menuBar.add(createStateButton);
-		menuBar.add(createCompositeStateButton);
-		menuBar.add(createFinalStateButton);
-		menuBar.add(flattenButton);
-		menuBar.add(validateButton);
+		menuBar.add(new JMenu("Test"));
 		frame.setJMenuBar(menuBar);
-		
 	}
 	
-	private void initializeEmptyGraph(mxGraph graphData){
-		graphView = new GraphView(graphData);
-		JPanel panel = new JPanel();
-		//panel.add(graph);
-		frame.add(graphView);
+	private void initializeEmptyGraph(){
+		
 	}
 
 }
