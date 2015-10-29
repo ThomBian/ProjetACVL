c151b8503829df7137cf3686704c73c8a6a526c9
Arthur De Kimpe <arthurdekimpe@gmail.com>
2015-10-29 23:12:07 +0100

    Disallowed final state to have outgoing transitions + corrected exceptions

--- src/main/java/view/CustomMxGraph.java
+++ src/main/java/view/CustomMxGraph.java
@@ -3,19 +3,41 @@ package view;
 import com.mxgraph.model.mxCell;
 import com.mxgraph.view.mxGraph;
 
-public class CustomMxGraph extends mxGraph{
+public class CustomMxGraph extends mxGraph {
 	// Make all edges unmovable
 	@Override
-	  public boolean isCellMovable(Object cell)
-	  {
-	    return !getModel().isEdge(cell);
-	  }	
-	  public boolean isValidDropTarget(Object cell, Object[] cells){
-		  // TODO : if cell  = null 
-		  if (cells.length == 1 && ((mxCell)cell).isVertex() && ((mxCell)cell).getStyle().equals(Style.COMPOSITE))
-		  {	  
-			  return true;
-		  }
-		  return false;
-	  }
+	public boolean isCellMovable(Object cell) {
+		return !getModel().isEdge(cell);
+	}
+
+	@Override
+	public boolean isValidDropTarget(Object cell, Object[] cells) {
+		// TODO : if cell = null
+		try{
+			if (cells.length == 1 && ((mxCell) cell).isVertex() && ((mxCell) cell).getStyle().equals(Style.COMPOSITE)) {
+				return true;
+			}
+			return false;
+		}catch(Exception e){
+			return false;
+		}
+	}
+
+	@Override
+	public boolean isValidConnection(Object source, Object target){
+		if(source.equals(target)) return true;
+		return true;
+	}
+	
+	// Disallow final state to have outgoing transition
+	@Override
+	public boolean isValidSource(Object cell){
+		try{
+			if(((mxCell) cell).getStyle().equals(Style.FINAL)) return false;
+			return true;
+		}catch(Exception e){
+			return false;
+		}
+		
+	}
 }
