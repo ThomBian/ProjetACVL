5df0955cc24ec6decf081a5808e449838c5ae776
Arthur De Kimpe <arthurdekimpe@gmail.com>
2015-11-05 13:40:10 +0100

    Added visitor stuff

---
+++ src/main/java/controller/visitor/FlattenVisitor.java
@@ -0,0 +1,34 @@
+package controller.visitor;
+
+import model.FinalState;
+import model.InitialState;
+import model.NamedState;
+import model.SimpleState;
+
+public class FlattenVisitor implements Visitor {
+
+	@Override
+	public void visit(SimpleState s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(NamedState s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(InitialState s) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void visit(FinalState s) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
